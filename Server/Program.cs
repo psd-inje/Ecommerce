20822bbf405c23313feee4918df1a772c760a32a
psd <psd@istaff.inje.ac.kr>
2022-02-12 16:14:44 +0900

    01 초기세팅

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,12 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "ContosoRecipes", Version = "v1" });
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ContosoRecipes v1"));
+
 }
 else
 {
